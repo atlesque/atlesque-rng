a95f4a98e1be6768986204c8aec7042e5f12522c
Alexander Van Maele <alexander@greenisland.be>
2023-11-19 12:35:23 +0100

    Change registerType to auto

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       injectRegister: 'auto',
+      registerType: 'autoUpdate',
       devOptions: {
         enabled: process.env.NODE_ENV === 'development',
       },
